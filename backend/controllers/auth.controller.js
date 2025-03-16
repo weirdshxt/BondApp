487699f629df33618466ac09a9726669664057e9
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-16 16:06:05 +0530

    added signup func

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,53 @@
-export const signup = (req, res) => {
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    
 
+    if(!fullName || !email || !password){
+      return res.status(400).json({message: "Please fill in all fields."});
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res
+        .status(201)
+        .json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+        });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
   } catch (err) {
-    
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
