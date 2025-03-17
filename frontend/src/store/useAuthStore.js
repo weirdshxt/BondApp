e752dfd2075162de0d155ca59adbb3bc776448f2
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-18 01:48:03 +0530

    build profile-page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -58,4 +58,18 @@ export const useAuthStore = create((set) => ({
       toast.error(err.response.data.message);
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (err) {
+      console.log("Error in update profile: ", err);
+      toast.error(err.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
