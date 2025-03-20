3272d2ace296dc20c87340dbb083e9c593ae709a
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-21 01:55:47 +0530

    completed project

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8080/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development"
-      ?  "http://localhost:8080": "/"
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
